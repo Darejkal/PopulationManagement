5b46bc35085cd527bedd7eeeabeb8f6de97d279c
Darejk <nguyendaian1945@gmail.com>
2024-06-20 23:46:59 +0700

    remove old models

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,9 @@ import express, { Express, Request, Response, NextFunction } from "express";
 import dbConnect from "./config/dbConnect";
 import bodyParser from "body-parser";
 import authRouter from "./routes/authRoute";
-import listRouter from "./routes/listRoute";
 import feeRouter from "./routes/feeRoute";
-import contributionRouter from "./routes/contributionRoute";
 import householdRouter from "./routes/householdRoute";
 import canBoRouter from "./routes/userRoute";
-import statisticRouter from "./routes/statisticRoute";
 
 const app: Express = express();
 const port: number = 5000;
@@ -33,12 +30,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // route api
 app.use("/api/auth", authRouter);
-app.use("/api/list", listRouter);
 app.use("/api/fees", feeRouter);
-app.use("/api/contributions", contributionRouter);
 app.use("/api/households", householdRouter);
 app.use("/api/user", canBoRouter);
-app.use("/api/statistics", statisticRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
