a1457dbe5c75e7d4f8182dbd8b3a84796717d06d
Darejk <nguyendaian1945@gmail.com>
2024-06-20 23:50:15 +0700

    remove old models

--- server/routes/individualRoute.ts
+++
@@ -1,9 +0,0 @@
-// import express, { Router } from "express";
-// import { authMiddleware, isAdmin } from "../middlewares/authMiddleware";
-// import { createIndividual } from "../controllers/IndividualController";
-
-// const router: Router = express.Router();
-
-// router.post("/create", authMiddleware, createIndividual);
-
-// export default router;
